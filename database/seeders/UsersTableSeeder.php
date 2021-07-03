9978594e13ef90862a877b606f893398906cd1b8
Đỗ Tiến Toàn <637766@sv.vnua.edu.vn>
2021-07-03 21:29:18 +0700

    update

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,7 +16,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->truncate();
-
         DB::table('users')->insert([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
